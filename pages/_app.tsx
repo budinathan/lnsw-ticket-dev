1fad6aed8eee9c2ec860fd22c5494ca3de189e79
Nathanael <Nathan@MacBook-Pro.local>
2023-08-07 08:57:51 +0700

    update layout background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,8 @@ import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className="bg-[#F8F9FA]">
-      <main className="bg-greybg max-w-7xl mx-auto relative">
+    <main className="bg-gray-300">
+      <main className="bg-greybg max-w-7xl mx-auto relative shadow-lg">
         <NextNProgress
           color="#29D"
           startPosition={0.3}
