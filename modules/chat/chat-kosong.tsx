a89988804ea8a994bb4fdaab52b14f46a441a41e
Nathanael <Nathan@MacBook-Pro.local>
2023-08-07 09:26:21 +0700

    add redirect feat

--- modules/chat/chat-kosong.tsx
+++ modules/chat/chat-kosong.tsx
@@ -1,11 +1,18 @@
 import Typography from "@/components/core/typography";
 import { HiChatAlt2 } from "@/constant/icons";
+import useForm from "@/hooks/get-form-client";
 
 export default function ChatKosong() {
+  const { formsFilter } = useForm();
+  console.log(formsFilter);
   return (
     <main className="flex justify-center items-center flex-col">
       <HiChatAlt2 className="text-[10rem] text-greybg" />
-      <Typography variant="small">Ketuk untuk memulai percakapan.</Typography>
+      {formsFilter.length > 0 ? (
+        <Typography variant="small">Ketuk untuk memulai percakapan.</Typography>
+      ) : (
+        <Typography variant="small">Anda belum mengajukan laporan</Typography>
+      )}
     </main>
   );
 }
