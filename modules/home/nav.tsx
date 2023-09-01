45f8ccf7699768c71bc28ceedd1b40b67e642a5f
Nathanael <Nathan@MacBook-Pro.local>
2023-09-01 10:15:26 +0700

    add icons on nav

--- modules/home/nav.tsx
+++ modules/home/nav.tsx
@@ -2,19 +2,27 @@ import Button from "@/components/core/button";
 import Typography from "@/components/core/typography";
 import HomeNav from "@/constant/home-db";
 import Link from "next/link";
+
+import { AiOutlineHistory, AiFillBook } from "react-icons/ai";
+import { HiChatAlt2 } from "react-icons/hi";
 export default function NavHome() {
   const { dash } = HomeNav;
 
   return (
     <>
       {dash.map((item, index) => {
+        const Icon = icons[index];
         return (
           <section
             key={index}
             className="h-fit flex flex-col gap-2 bg-white  shadow-lg rounded-md"
             style={{ padding: "1rem" }}
           >
-            <Typography variant="large">{item.title}</Typography>
+            <div className="flex justify-between items-center">
+              <Typography variant="large">{item.title}</Typography>
+              <Icon icon={icons[index]} className="text-2xl" />
+            </div>
+
             <Typography variant="small">{item.desc}</Typography>
             <Link href={item.link}>
               <Button variant="default" size="default" className="w-full">
@@ -27,3 +35,5 @@ export default function NavHome() {
     </>
   );
 }
+
+const icons = [AiOutlineHistory, AiFillBook, HiChatAlt2];
